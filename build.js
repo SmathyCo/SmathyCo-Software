f23c6ab51185c7c0cda9d92d298207c54084d10d
MathisSparky <SmathyCo@gmail.com>
2024-09-06 17:36:29 -0400

    First new version.

--- build.js
+++ build.js
@@ -8,7 +8,7 @@ if (process.argv[2]) {
         console.error("Can't commit, nothing changed in the project.");
         process.exit(1);
     }
-    execSync("git push -u origin main --force");
+    execSync("git push -u origin master --force");
 } else {
     console.log("Please provide a commit message.");
 }
