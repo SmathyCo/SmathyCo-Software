002dd3707be2692e15aa55c73d02a75e4eaff860
MathisSparky <SmathyCo@gmail.com>
2024-09-06 17:33:05 -0400

    First new version.

---
+++ build.js
@@ -0,0 +1,14 @@
+const { execSync } = require("child_process");
+
+if (process.argv[2]) {
+    execSync("git add -A");
+    try {
+        execSync(`git commit -m "${process.argv[2]}"`);
+    } catch(err) {
+        console.error("Can't commit, nothing changed in the project.");
+        process.exit(1);
+    }
+    execSync("git push -u origin main --force");
+} else {
+    console.log("Please provide a commit message.");
+}
